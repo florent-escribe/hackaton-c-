f72f6425f868442a16741fe1e2d29e49bbe9cd26
19michel <55029793+19michel@users.noreply.github.com>
2020-01-31 16:51:49 +0100

    Ajout de l'inventaire

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 #include <ncurses.h>
 #include "functions.hpp"
+#include "inventaire.hpp"
+
 
 int main(void) {
+
+    Inventaire i {};
 
     cbreak();    
     keypad(stdscr, TRUE);
@@ -13,17 +17,35 @@ int main(void) {
     int y_pos = 17;
 
     move(x_pos,y_pos);
+    start_color();
+	init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+	attron(COLOR_PAIR(1));
     addch('@');
     refresh();
 
 
-    while(1) {
-            move ();
+    //while(1) {
+    //        move ();
         
-    }
+    //}
 
+    char c = getch();
+    if (c == 'i') {    
+        mvwin(stdscr,50,50);
+        WINDOW* w=newwin(50,50,0,0);
+        wmove(w,0,0);
+        wrefresh(w);
+        i.print(w);
+        wrefresh(w);
+        getch();
+        wclear(w);
+        delwin(w);
+        mvwin(stdscr,0,0);
+        refresh();
+    };
     getch();
     endwin();
+
 
     return 0;
 }
