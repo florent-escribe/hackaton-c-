17950de092bd10960514b1bf7b5dfe3b0fa560f4
florent-escribe <florent.escribe@gmail.com>
2020-01-31 13:30:34 +0100

    move

--- main.cpp
+++ main.cpp
@@ -18,27 +18,7 @@ int main(void) {
 
 
     while(1) {
-            char c = getch();
-            if(c == 'z') {      //TOP
-                mvaddch(x_pos,y_pos,'.');
-                x_pos-=1;
-                mvaddch(x_pos,y_pos, '@');
-            };
-            if(c == 's') {      //BOTTOM
-                mvaddch(x_pos,y_pos,'.');
-                x_pos+=1;
-                mvaddch(x_pos,y_pos, '@');
-            };
-            if(c == 'q'){       //LEFT
-                mvaddch(x_pos,y_pos,'.');
-                y_pos-=1;
-                mvaddch(x_pos,y_pos, '@');
-            };
-            if(c == 'd') {      //RIGHT
-                mvaddch(x_pos,y_pos,'.');
-                y_pos+=1;
-                mvaddch(x_pos,y_pos, '@');
-            }
+            move ();
         
     }
 
